c38bb92398835b4a4ea84c1bbe34cf68dc6ea5a3
EduardoMoreaes <17eduardo05@gmail.com>
2023-04-26 12:50:41 -0300

    modified:   R/app.R 	deleted:    R/packages.R 	new file:   R/server.R 	new file:   R/ui.R

--- R/app.R
+++ R/app.R
@@ -1,5 +1,16 @@
-library(devtools)
-library(geobage)
-library(shiny)
-library(leaflet)
+pacotes <- list("devtools", "geobage", "shiny", "leaflet")
+
+for(i in seq_along(pacotes)){
+    pacote <- pacotes[[i]]
+    if (pacote %in% installed.packages()) {
+          print(paste("Pacote instalado...", pacote))
+          print(paste("Carregando...", pacote))
+          library(pacote,  character.only = TRUE)
+    } else {
+          print(paste("Instalando", pacote, "..."))
+          install.packages(pacote)
+    }
+}
+
+
 runApp()
--- R/packages.R
+++
@@ -1,4 +0,0 @@
-install.packages("devtools")
-install.packages("shiny")
-utils::remove.packages('geobage')
-devtools::install_github("GeoInformacao/geobage")
