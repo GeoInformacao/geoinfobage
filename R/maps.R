b74482c2aaa84ba2b7b70fae3cf15794a41166cf
EduardoMoreaes <17eduardo05@gmail.com>
2023-06-07 15:29:12 -0300

    Contorno

--- R/maps.R
+++ R/maps.R
@@ -1,6 +1,7 @@
 map_shape_bg <- function(data, label){
   leaflet(data) %>% addTiles()%>%
-    addPolygons(color = "darkblue", label = ~label, weight = 1, opacity = 0.5)
+    addPolygons(color = "darkblue", label = ~label, weight = 1, opacity = 0.5) %>%
+    addPolygons(data = read_shape_bg(), weight = 2, opacity = 1, color = 'green', fill = FALSE)
 }
 map_points_bg <- function(data){
   leaflet(data) %>% addTiles()%>%
